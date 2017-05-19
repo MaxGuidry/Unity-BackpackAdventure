93e4cb26b51f57c35e7dece6fc278c0bddef8890
MaxGuidry <mguidry7@outlook.com>
2017-05-19 09:50:39 -0500

    testing GUILayout

--- Assets/Scripts/LootTable.cs
+++ Assets/Scripts/LootTable.cs
@@ -19,7 +19,7 @@ public class LootTable : ScriptableObject
 
     public LootTable(List<Item> items)
     {
-        
+
     }
     public List<ItemDrop> Drops;
     [SerializeField]
@@ -42,9 +42,33 @@ public class LootTable : ScriptableObject
         string s = "";
         public override void OnInspectorGUI()
         {
+            string[] t = new[] { "test1", "Test2" };
+
+            //I dont know how to change the selected button from the inspector by clicking/dont know how to access buttons
+            GUILayout.SelectionGrid(0, t, 2);
+
+            //adds empty space with whatever float of pixels is passed in
+            GUILayout.Space(20f);
+
+            //dont see a difference between TextArea and TextField
+            GUILayout.TextArea("nig");
+            GUILayout.TextField("nog");
+
+            //almost the same as a SelectionGrid dont know how to change selected index they are buttons but I dont know how to access them.
+            GUILayout.Toolbar(1, t);
+
+            //idk how this even works
+            //GUILayout.Window(1, new Rect(1, 1, 10, 10),new GUI.WindowFunction();
+
+            //idk what the purpose of this one is
+            EditorGUILayout.Vector2Field("Vector2Field test", new Vector2(2, 2));
+
+            
+
             List<Item> items = new List<Item>();
             if (GUILayout.Button("Roll"))
             {
+
                 s = "";
                 var test = target as LootTable;
                 var rngDrops = test.GetDrops();
@@ -58,12 +82,12 @@ public class LootTable : ScriptableObject
                 }
 
             }
-           
+
             foreach (var item in items)
             {
                 s += item.name + ", ";
             }
-            EditorGUILayout.TextField("Items Dropped",s);
+            EditorGUILayout.TextField("Items Dropped", s);
 
             base.OnInspectorGUI();
         }
